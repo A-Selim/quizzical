4c49cc974ca396f80d7e1c93ab0bc925af89f7ea
Ahmed Selim <65416364+A-Selim@users.noreply.github.com>
2022-09-28 16:55:19 +0200

    Create quiz logic

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -13,8 +13,16 @@ export function Question(props) {
 export function Choice(props) {
   return (
     <>
-      <input type="radio" name={props.questionId} className="choice-radio" id={props.answerId} value={props.text} />
-      <label className="choice" htmlFor={props.answerId}>
+      <input
+        type="radio"
+        name={props.questionId}
+        className="choice-radio"
+        id={props.answerId}
+        value={props.text}
+        onChange={(event) => props.select(event)}
+        checked={props.checkedValue === props.text}
+      />
+      <label className={`choice ${props.showCorrect ? "correct-choice" : ""}`} htmlFor={props.answerId}>
         {props.text}
       </label>
     </>
--- src/components/questionsPage/QuestionsPage.jsx
+++ src/components/questionsPage/QuestionsPage.jsx
@@ -2,35 +2,108 @@ import React from "react";
 import Button from "../button/Button";
 import { Question, Choice } from "../question/Question";
 import "./questionsPage.css";
-import data from "../../data";
 import { nanoid } from "nanoid";
 
 export default function QuestionsPage() {
-  const questionsArr = data.results.map((result) => {
-    return {
-      id: nanoid(10),
-      question: result.question,
-      answers: [result.correct_answer, ...result.incorrect_answers],
-    };
-  });
+  const [questions, setQuestions] = React.useState([]);
+  const [isAnswerShow, setIsAnswerShow] = React.useState(false);
+  const [score, setScore] = React.useState(0);
+  const [playAgain, setPlayAgain] = React.useState(false);
 
-  const questionWrapperElements = questionsArr.map((element) => {
+  React.useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=multiple");
+      const data = await res.json();
+      setQuestions(
+        data.results.map((result) => {
+          return {
+            id: nanoid(10),
+            question: result.question,
+            answers: [result.correct_answer, ...result.incorrect_answers].sort(() => Math.random() - 0.5),
+            correctAnswer: result.correct_answer,
+            userAnswer: "",
+            showCorrect: false,
+          };
+        })
+      );
+    }
+    fetchData();
+  }, [playAgain]);
+
+  const questionWrapperElements = questions.map((element) => {
     return (
       <div className="question-wrapper" key={nanoid(10)}>
         <Question text={element.question} />
         <div className="answers-wrapper">
           {element.answers.map((answer) => (
-            <Choice questionId={element.id} answerId={nanoid(10)} text={answer} key={nanoid(10)} />
+            <Choice
+              questionId={element.id}
+              answerId={nanoid(10)}
+              text={answer}
+              key={nanoid(10)}
+              select={handleSelect}
+              checkedValue={element.userAnswer}
+              showCorrect={answer === element.correctAnswer && element.showCorrect}
+            />
           ))}
         </div>
       </div>
     );
   });
 
+  function handleSelect(event) {
+    if (!isAnswerShow) {
+      setQuestions((oldQuestions) =>
+        oldQuestions.map((question) => {
+          return question.id === event.target.name
+            ? {
+                ...question,
+                userAnswer: event.target.value,
+              }
+            : question;
+        })
+      );
+    }
+  }
+
+  function handleCheck() {
+    if (!isAnswerShow) {
+      setQuestions((oldQuestions) =>
+        oldQuestions.map((question) => {
+          return {
+            ...question,
+            showCorrect: true,
+          };
+        })
+      );
+
+      questions.forEach((question) => {
+        question.userAnswer === question.correctAnswer
+          ? setScore((oldScore) => oldScore + 1)
+          : setScore((oldScore) => oldScore + 0);
+      });
+    }
+
+    setIsAnswerShow(true);
+  }
+
+  function resetQuiz() {
+    setIsAnswerShow(false);
+    setScore(0);
+    setPlayAgain((prevState) => !prevState);
+  }
+
   return (
     <>
       {questionWrapperElements}
-      <Button text="Check answers" size="medium" />
+      {isAnswerShow ? (
+        <div className="result-wrapper">
+          <p className="result-text">You scored {score}/5 correct answers</p>
+          <Button text="Play again" size="small" action={resetQuiz} />
+        </div>
+      ) : (
+        <Button text="Check answers" size="medium" action={handleCheck} />
+      )}
     </>
   );
 }
