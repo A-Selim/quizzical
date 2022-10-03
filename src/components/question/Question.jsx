15348eea1a0588d34b980029bb7050eba6d0d980
Ahmed Selim <65416364+A-Selim@users.noreply.github.com>
2022-10-03 18:13:56 +0200

    Refactor UI

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -2,10 +2,5 @@ import React from "react";
 import "./Question.css";
 
 export default function Question(props) {
-  return (
-    <p className="question">
-      {props.text}
-      {props.text[props.text.length - 1] !== "?" && "?"}
-    </p>
-  );
+  return <p className="question">{props.text}</p>;
 }
